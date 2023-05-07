41a0aefcf092b054c5dd3d2a48a02a34af15dad5
@hexlet/cli <support@hexlet.io>
2023-05-08 06:05:35 +0700

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -10,14 +10,7 @@ class App {
                 .filter(mail -> emails.contains("gmail.com"))
                 .count();
 
-        long count2 = emails.stream()
-                .filter(mail -> emails.contains("yandex.ru"))
-                .count();
-
-        long count3 = emails.stream()
-                .filter(mail -> emails.contains("hotmail.com"))
-                .count();
-        var result = count1 + count2 + count3;
+        var result = count1;
 
         return result;
     }
